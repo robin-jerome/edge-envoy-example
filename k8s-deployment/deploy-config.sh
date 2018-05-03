5976f96e6d396d6ccda8b4eb473eca8b62bc137c
rjerome <rjerome@architech.ca>
2018-05-03 16:57:45 -0400

    Runtime traffic shifting

--- k8s-deployment/deploy-config.sh
+++ k8s-deployment/deploy-config.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-kubectl delete configmap edge-envoy-config --namespace edge-envoy
-
 if [ "$#" -ne 1 ] ; then
   echo "Usage: $0 </path/to/envoy-config-file.json>" >&2
   echo "assuming the traffic configuration is \"simple load balancing\""
@@ -10,4 +8,7 @@ if [ "$#" -ne 1 ] ; then
   config_file=$1
 fi
 
-kubectl create configmap edge-envoy-config --from-file=envoy.json=$config_file --namespace edge-envoy
+rt_config_file="../envoy/runtime/routing_ts_node-app_latest"
+
+kubectl create configmap edge-envoy-config --from-file=envoy.json=$config_file --namespace edge-envoy -o yaml --dry-run | kubectl apply -f -
+kubectl create configmap edge-envoy-rt-config --from-file=latest=$rt_config_file --namespace edge-envoy -o yaml --dry-run | kubectl apply -f -
