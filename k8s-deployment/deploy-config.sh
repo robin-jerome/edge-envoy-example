4a8768dbaa78652cfb84aedeb0e41858bfa34d7c
rjerome <rjerome@architech.ca>
2018-05-03 12:49:57 -0400

    Working version of Campaign deployment

--- k8s-deployment/deploy-config.sh
+++ k8s-deployment/deploy-config.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-kubectl create configmap edge-envoy-config --from-file=envoy.json=../envoy/envoy.json --namespace edge-envoy
+kubectl delete configmap edge-envoy-config --namespace edge-envoy
+
+if [ "$#" -ne 1 ] ; then
+  echo "Usage: $0 </path/to/envoy-config-file.json>" >&2
+  echo "assuming the traffic configuration is \"simple load balancing\""
+  config_file="../envoy/envoy.json"
+ else
+  config_file=$1
+fi
+
+kubectl create configmap edge-envoy-config --from-file=envoy.json=$config_file --namespace edge-envoy
